839e53052c8ad4dd50166c392fdafedb693de566
Carlos Sanchez <xt3426@rimac.com.pe>
2021-04-07 16:05:07 -0500

    [ADD] agregado el build, a los archivos a subir

---
+++ build/precache-manifest.96252f90c451c35d2d36dc8ca22821f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ddcb35b65dab23b6a3aa6ee10d1ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "167b1122be79c55f540c",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "b26855c563ecf771ae6f",
+    "url": "/static/js/2.421bde4e.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "/static/js/2.421bde4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "167b1122be79c55f540c",
+    "url": "/static/js/main.7dde2bd9.chunk.js"
+  },
+  {
+    "revision": "704584960f240e3f1674",
+    "url": "/static/js/runtime-main.9955b4b2.js"
+  },
+  {
+    "revision": "2a485c251b9b56c4fbb2a7fb88f8b10c",
+    "url": "/static/media/react-logo.2a485c25.png"
+  }
+]);
